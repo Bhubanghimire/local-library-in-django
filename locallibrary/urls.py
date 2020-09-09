4feb226a1c9ea3a3e5cb589501d9af3522f84f94
Bhuban Ghimire <bhubanghimire3130@gmail.com>
2020-09-09 11:16:19 +0545

    template url commit

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('catalog.urls')),
+    path('catalog/',include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 
 ]
